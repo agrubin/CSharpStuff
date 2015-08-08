c335e60e666d0653d9fbf427a53b362a3983f176
Al Rubin <agrubin@hotmail.com>
2015-08-08 19:54:52 -0400

    Added string intern test

--- CSharpStuff/Program.cs
+++ CSharpStuff/Program.cs
@@ -9,7 +9,72 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        int x;
+        public static void InternString()
+        {
+            // 1.
+            // Create string at runtime.
+            StringBuilder b = new StringBuilder().Append("cat ").Append(
+                "and ").Append("dog");
+            // 2.
+            // Get runtime string.
+            string s1 = b.ToString();
+            // 3.
+            // Get string pool reference to string.
+            string s2 = string.Intern(s1);
+
+            // 4.
+            // Three loops:
+            // - Repeat benchmark 10 times
+            // - Repeat inner loop 10000 times
+            // - Repeat 2 tests 10000 times
+            int m = 10000;
+            for (int v = 0; v < 10; v++)
+            {
+                int d = 0;
+                long t1 = Environment.TickCount;
+
+                // 5.
+                // Test regular string.
+                for (int i = 0; i < m; i++)
+                {
+                    for (int a = 0; a < m; a++)
+                    {
+                        if (s1 == "cat and rabbit")
+                        {
+                            d++; // false
+                        }
+                        if (s1 == "cat and dog")
+                        {
+                            d--; // true
+                        }
+                    }
+                }
+                long t2 = Environment.TickCount;
+
+                // 6.
+                // Test interned string.
+                for (int i = 0; i < m; i++)
+                {
+                    for (int a = 0; a < m; a++)
+                    {
+                        if (s2 == "cat and rabbit")
+                        {
+                            d++; // false
+                        }
+                        if (s2 == "cat and dog")
+                        {
+                            d--; // true
+                        }
+                    }
+                }
+                // 7.
+                // Write results.
+                long t3 = Environment.TickCount;
+                Console.Write((t2 - t1));
+                Console.WriteLine("," + (t3 - t2));
+            }
+            Console.ReadLine();
+        }
 
         //
         // This is a simple way of implementing a sort of "inline lambda expression".
@@ -33,8 +98,15 @@ namespace ConsoleApplication1
         }
         static void Main(string[] args)
         {
+            //
+            // Try inline Lambda.
+            //
+
             bool foo_equals_bar = Functional.Lambda<string, bool>(str => str.Equals("foo"))("bar");
             bool btest = Functional.Lambda(() => false)();
+
+            InternString();
+            
         }
     }
 }
